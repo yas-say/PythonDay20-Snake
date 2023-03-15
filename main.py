3cbcad3578d62550df71505cc9a2bfb2de6a99aa
yas-say <yasowant@live.com>
2023-03-14 22:45:25 -0400

    updated scoreboard.py class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from turtle import Turtle, Screen
 from snake import Snake
 from food import Food
-from scoreboard import Score
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -20,7 +20,7 @@ screen.tracer(0)
 # t3.setpos(x=t2.xcor()-20, y=0)
 snake = Snake()
 food = Food()
-score = Score()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
--- scoreboard.py
+++ scoreboard.py
@@ -1,19 +1,17 @@
 from turtle import Turtle
 
-class Score(Turtle):
+
+class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("white")
-        self.shape()
-        self.penup()
         self.score = 0
-        self.goto(-25,280)
         self.hideturtle()
-
-    def print_score(self):
-        self.write(f"Score: {self.score}", move=False, align='left', font=('Verdana', 12, 'normal'))
+        self.color("white")
+        self.write(f"Score: {self.score}", move=False, align='center', font=('Verdana', 12, 'normal'))
+        self.penup()
+        self.goto(-25, 280)
 
     def increment_score(self):
-        self.score+=1
+        self.score += 1
         self.clear()
         self.write(f"Score: {self.score}", move=False, align='left', font=('Verdana', 12, 'normal'))
